c791d8647119d6430ad05f559369159a048a1c23
李金松 <lijinsong02@58.com>
2025-05-13 15:13:17 +0800

    test(pool/syncPool): 添加泛型类型的 SyncPool 测试用例
    - 新增测试用例以验证泛型类型的 SyncPool 行为
    - 添加 node 泛型结构用于测试
    - 实现了以下测试场景：
      - 同类型泛型的 SyncPool 应该相同
      - 不同类型泛型的 SyncPool 应该不同

--- pool/syncPool/sync_pool_test.go
+++ pool/syncPool/sync_pool_test.go
@@ -6,10 +6,28 @@ import (
 	"testing"
 )
 
+type node[T any] struct {
+	val T
+}
+
 func TestNewSyncPool_same_pool_when_type_is_same(t *testing.T) {
 	syncPool1 := NewSyncPool[int]()
 	syncPool2 := NewSyncPool[int]()
 	assert.Equal(t, syncPool1, syncPool2)
+}
+
+func TestNewSyncPool_same_pool_when_Generics_type_is_same(t *testing.T) {
+	syncPool1 := NewSyncPool[node[string]]()
+	syncPool2 := NewSyncPool[node[string]]()
+	assert.Equal(t, syncPool1, syncPool2)
+}
+
+func TestNewSyncPool_not_same_pool_when_Generics_type_is_not_same(t *testing.T) {
+	syncPool1 := NewSyncPool[node[string]]()
+	syncPool2 := NewSyncPool[node[int]]()
+	assert.NotEqual(t, syncPool1, syncPool2)
+	syncPool3 := NewSyncPool[node[int64]]()
+	assert.NotEqual(t, syncPool3, syncPool2)
 }
 
 func TestSyncPool_Get_reuse_object(t *testing.T) {
