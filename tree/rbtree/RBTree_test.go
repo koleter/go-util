5db531baea63a3892db181187be88add310e6d71
李金松 <lijinsong02@58.com>
2025-05-15 17:15:03 +0800

    test(rbtree): 反向遍历红黑树节点
    - 在 RBTree_test.go 中添加了反向遍历红黑树节点的测试用例
    - 通过 tree.HighestNode() 获取最大值节点，然后使用 node.Prev()逐个遍历前驱节点- 验证反向遍历结果是否与预期的降序数组一致

--- tree/rbtree/RBTree_test.go
+++ tree/rbtree/RBTree_test.go
@@ -298,4 +298,10 @@ func TestRange(t *testing.T) {
 		slice = append(slice, node.Key)
 	}
 	assert.Equal(t, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}, slice)
+
+	slice = slice[:0]
+	for node := tree.HighestNode(); node != nil; node = node.Prev() {
+		slice = append(slice, node.Key)
+	}
+	assert.Equal(t, []int{19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, slice)
 }
